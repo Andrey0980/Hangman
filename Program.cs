456cf9f4450d8a730d8ba920b00af05717b3cd7c
Andrey0980 <ap6258738@gmail.com>
2023-09-04 01:49:03 +0300

    Update 2.0

--- Program.cs
+++ Program.cs
@@ -1,18 +1,122 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Xml.Linq;
 
+Random random = new Random();
+
 byte death = 0;
-string guessed_word;
+string guessed_word = "";
 string player_letter = "";
 bool game_end = false;
 
-string[] words_to_guess = { "apple", "window", "toothbrush", "book", "chair", "shoe", "stove", "banana", "morning", "school", "pencil", "knife", "camera", "microphone", "headphones", "toaster", "microwave", "toothbrush", "laptop", "phone", "mirror", "television", "flower", "washboard", "cobweb", "inkwell", "fipple", "quodlibet", "objurgate", "pandiculation", "gaberlunzie", "gallimaufry" };
-string[] guessed_letters = {};
+string lang = "en";
+
+string[] words_to_guess = { "apple", "window", "toothbrush", "book", "chair", "shoe", "stove", "morning", "school", "pencil", "knife", "camera", "microphone", "headphones", "toaster", "microwave", "toothbrush", "laptop", "phone", "mirror", "television", "flower", "washboard", "cobweb", "inkwell", "fipple", "quodlibet", "objurgate", "pandiculation", "gaberlunzie", "gallimaufry" };
+string[] words_to_guess_ru = { "яблоко", "окно", "щётка", "книга", "стул", "обувь", "печь", "утро", "школа", "карандаш", "нож", "камера", "микрофон", "наушники", "тостер", "микроволновка", "ноутбук", "телефон", "зеркало", "телевизор", "цветок", "доска", "явства", "чернильница", "русич", "катастрофа", "бюрократство", "пустомеля", "терем", "чашница", "супостат", "золотник" };
+
+string[] guessed_letters = new string[4];
+
+void ChangeLang()
+{
+    while (true)
+    {
+        Console.WriteLine("Select language\nEnglish - en\nRussian - ru");
+        lang = Convert.ToString(Console.ReadLine());
+
+        if (lang == "en" || lang == "ru") break;
+        else
+        {
+            Console.Clear();
+            continue;
+        }
+    }
+}
 
 void MakeWord()
 {
-    guessed_word = words_to_guess[new Random().Next(words_to_guess.Length)];
+    if (lang == "en")
+    {
+        guessed_word = words_to_guess[new Random().Next(words_to_guess.Length)];
+
+        if (guessed_word.Length < 6) // 5 and less.
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+        else if (guessed_word.Length > 5 && guessed_word.Length < 9) // 6 - 8
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+        else if (guessed_word.Length > 8 && guessed_word.Length < 11) // 9 - 10
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+        else if (guessed_word.Length > 10) // 11 and more
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+    }
+    if (lang == "ru")
+    {
+        guessed_word = words_to_guess_ru[new Random().Next(words_to_guess_ru.Length)];
+
+        if (guessed_word.Length < 6) // 5 and less.
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+        else if (guessed_word.Length > 5 && guessed_word.Length < 9) // 6 - 8
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+        else if (guessed_word.Length > 8 && guessed_word.Length < 11) // 9 - 10
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+        else if (guessed_word.Length > 10) // 11 and more
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int randomIndex = random.Next(0, guessed_word.Length);
+                char randomChar = guessed_word[randomIndex];
+                guessed_letters[i] = randomChar.ToString();
+            }
+        }
+    }
 }
 
 void PrintHang(int mistakes)
@@ -103,9 +207,11 @@ void PrintHang(int mistakes)
     if (mistakes == 7)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(" You lose...");
+        if (lang == "en") Console.WriteLine(" You lose...");
+        if (lang == "ru") Console.WriteLine(" Ты проиграл...");
         Console.ResetColor();
-        Console.WriteLine($" Word is: {guessed_word}");
+        if (lang == "en") Console.WriteLine($" Word is: {guessed_word}");
+        if (lang == "ru") Console.WriteLine($" Слово было: {guessed_word}");
         game_end = true;
     }
 }
@@ -144,21 +250,41 @@ void CheckWin()
     {
         PrintHang(death);
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(" You win!!!");
+        if (lang == "en") Console.WriteLine(" You win!!!");
+        if (lang == "ru") Console.WriteLine(" Ты победил!!!");
         Console.ResetColor();
         game_end = true;
     }
 }
+void resetvars()
+{
+    death = 0;
+    guessed_word = "";
+    player_letter = "";
+}
 
 // Game
+ChangeLang();
+
 MakeWord();
 
-while (game_end == false)
+while (true)
 {
-    PrintHang(death);
+    while (game_end == false)
+    {
+        PrintHang(death);
 
-    player_letter = Convert.ToString(Console.ReadLine());
-    CheckLetterInWord();
+        player_letter = Convert.ToString(Console.ReadLine());
+        CheckLetterInWord();
 
-    CheckWin();
+        CheckWin();
+    }
+
+    if (game_end == true)
+        if (lang == "en") Console.WriteLine("Play again?");
+        if (lang == "ru") Console.WriteLine("Ещё раз?");
+    Console.ReadLine();
+    game_end = false;
+    resetvars();
+    MakeWord();
 }
--- README.md
+++ README.md
@@ -5,8 +5,12 @@
 ## v1.0
 [Added]
  - Added mechanics of word guessing.
- - Added mechanics of winning and losing
+ - Added mechanics of winning and losing.
 
 ## v1.5
 [Added]
- - Added some of the letters in the words have already been guessed
+ - Added some of the letters in the words have already been guessed.
+
+## v2.0
+[Added]
+ - Added possibility to continue the game after victory / defeat.
